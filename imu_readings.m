4262714275ddf45cbf63751128b630879a17944b
jacksonwhite19 <105457994+jacksonwhite19@users.noreply.github.com>
2022-05-18 00:40:29 -0500

    Update imu_readings.m

--- imu_readings.m
+++ imu_readings.m
@@ -8,8 +8,3 @@ adjust = calibration;
 x_adjust = adjust(1,1);
 y_adjust = adjust(1,2);
 z_adjust = adjust(1,3);
-
-%take new, calibrated readings
-x = position(1,1) - x_adjust;
-y = position(1,2) - y_adjust;
-z = position(1,3) - z_adjust;
